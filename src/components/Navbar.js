da1146dbb71232b49d09b8e913fa5699fd2a5376
levit <levit.osei-wusu@gtbank.com>
2021-09-30 17:18:53 +0000

    Adds white logos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ export const Navbar = () => {
         <nav>
             <ul className="flex space-x-12">
                 <li>
-                    <img src="github-logo.png" alt="github" className="w-16 h-16 transition duration-500 ease-in-out transform hover:scale-125"/>
+                    <img src="github.png" alt="github" className="w-16 h-16 transition duration-500 ease-in-out transform hover:scale-125"/>
                 </li>
                 <li>
                     <img src="twitter.png" alt="twitter" className="w-16 h-16 transition duration-500 ease-in-out transform hover:scale-125"/>
