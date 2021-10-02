424c21eeee331163110913679a37fd733012dff1
Levit Osei Kanner <lkanner21@gmail.com>
2021-10-02 11:19:07 +0000

    Adds changes

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,8 +7,6 @@
 src/App.css,a/a/aa7165aefcf8ee3d73539e99ab007daf1aa18d8d
 H
 src/components/Avatar.js,5/6/56e1055f2418b236ac284798df739f5169d74056
-<
-package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 H
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
-import {useState} from 'react';
-import React, { useRef } from 'react';
+import {useState, useRef} from 'react';
 import emailjs from 'emailjs-com';
 
 function TextField({title, type, value, onChange}) {
@@ -8,6 +7,7 @@ function TextField({title, type, value, onChange}) {
         <input type={type ?? 'text'} name={title.replaceAll(" ", '').toLowerCase()}
                id={title.replaceAll(" ", '').toLowerCase()}
                autoComplete="on" value={value}
+               required
                onChange={onChange} placeholder={title}
                className="w-full p-3 placeholder-black rounded-md border border-gold-500"/>
     </div>;
@@ -17,35 +17,41 @@ export const Contact = ({hideForm}) => {
     const [fullName, setFullName] = useState('')
     const [message, setMessage] = useState('')
     const [email, setEmail] = useState('')
+    const [sent, setSent] = useState(false)
 
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
+        if (!fullName && !message && !email) return
         emailjs.sendForm(process.env.REACT_APP_SERVICEID, process.env.REACT_APP_TEMPLATEID, form.current, process.env.REACT_APP_USERID)
             .then((result) => {
-                console.log(result.text, 'message sent');
-                hideForm()
+                setSent(true);
+                setTimeout(hideForm, 2000)
             }, (error) => {
                 console.log(error.text);
             });
     };
 
     return <div className="absolute inset-0 flex items-center justify-center">
-        <div className="bg-white px-6 md:px-8 pb-8 rounded-lg max-w-md sm:max-w-xl mx-auto relative shadow-lg border border-gold-500 ">
+        <div
+            className="bg-white px-6 md:px-8 pb-8 rounded-lg max-w-md sm:max-w-xl mx-auto relative shadow-lg border border-gold-500 ">
             <h2 className="text-center text-3xl text-gold-500 font-bold py-8"> Send me a message </h2>
-            <form ref={form} onSubmit={sendEmail} className="space-y-4 text-black" >
+            <form ref={form} onSubmit={sendEmail} className="space-y-4 text-black">
                 <TextField title="Full Name" value={fullName} onChange={e => setFullName(e.target.value)}/>
                 <TextField type="email" title="Email" value={email} onChange={e => setEmail(e.target.value)}/>
                 <div>
                     <label htmlFor="message" className="block py-2 text-gold-500"> Message </label>
-                    <textarea id="message" name="message" value={message} onChange={e => setMessage(e.target.value)} rows={5}
+                    <textarea id="message" name="message" value={message} onChange={e => setMessage(e.target.value)}
+                              rows={5}
+                              required
                               className="w-full p-3 rounded-md border border-gold-500">
                 </textarea>
                 </div>
                 <button type="submit"
-                        className="bg-gold-500 text-black text-md font-semibold w-full py-2 rounded-md"> Send
-                    Message
+                        className="bg-gold-500 text-black text-md font-semibold w-full py-2 rounded-md">
+                    {!sent && <span> Send Message</span>}
+                    {sent && <span>Message sent</span>}
                 </button>
             </form>
             <button onClick={hideForm}
