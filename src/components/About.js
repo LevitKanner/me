7a73a3da47c8f3710a0c14387a526a39f7cb21bd
Levit Osei Kanner <lkanner21@gmail.com>
2021-10-31 16:35:12 +0000

    Edits about

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ export const About = () => {
         <h1 className="text-4xl py-4 font-royale"> <span > Hi, </span>   <span className="block font-dancing text-gold-500">I'm Levit </span></h1>
         <p className="text-md md:text-xl pt-4 tracking-wider leading-relaxed">
         <span className="block py-2">A Software Developer based in Ghana. I have a serious passion for solving problems, developing and creating software applications.</span>
-        <span className="block py-2">Well-organised person, problem solver, independent employee with high attention to detail.</span>
+        <span className="block py-2">Well-organised person, problem solver, independent with high attention to detail.</span>
         <span className="block py-2">Interested in the entire development spectrum and working on ambitious projects with positive people.</span>
     </p>
     </div>
